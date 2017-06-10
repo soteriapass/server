a796dc16b3704c4cda5338a51b534087b78bac7c
viperman1271 <viperman1271@hotmail.com>
2017-06-10 12:22:04 -0400

    Added sig segmentation to the main.cpp handled signals

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,7 @@ void signal_callback_handler(int signum)
 int main(int argc, char** argv)
 {
     signal(SIGINT, signal_callback_handler);
+    signal(SIGSEGV, signal_callback_handler);
 
     if(argc > 1 && strcmp(argv[1], "-v") == 0)
     {
